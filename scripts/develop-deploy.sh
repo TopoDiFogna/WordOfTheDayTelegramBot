a500b5368be7b8689c4aed65aecea4209c9ca612
Paolo Paterna <paolo.paterna@gmail.com>
2019-04-23 21:36:10 +0200

    Miswritten if ocndition + debug

--- scripts/develop-deploy.sh
+++ scripts/develop-deploy.sh
@@ -3,5 +3,6 @@
 set -e
 
 if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
+    echo "Well not a PR so i can try to push";
     docker push "$DOCKER_USERNAME"/wordofthedaytelegrambot;
 fi;
--- scripts/master-deploy.sh
+++ scripts/master-deploy.sh
@@ -3,7 +3,11 @@
 set -e
 
 if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then
-    if [[ -z "$TRAVIS_TAG" ]]; then
+    echo "Well not a PR so i can try to push";
+    if [[ -n "$TRAVIS_TAG" ]]; then
+        echo "Found tag: $TRAVIS_TAG pushing to docker hub a tagged image";
         docker push "$DOCKER_USERNAME"/wordofthedaytelegrambot:"$TRAVIS_TAG";
+    else
+        echo "Commit not tagged, will not push the image to docker hub";
     fi;
 fi;
